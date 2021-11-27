692db1c507a555dc609e097b03c0a813087f6eb1
Junling <40445944+junling2@users.noreply.github.com>
2021-11-26 16:25:53 -0800

    Add logging functionality

--- selenium_tests/src/test/java/com/selenium_tests/testCases/TCBaseClass.java
+++ selenium_tests/src/test/java/com/selenium_tests/testCases/TCBaseClass.java
@@ -1,5 +1,7 @@
 package com.selenium_tests.testCases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -10,11 +12,15 @@ public class TCBaseClass {
     public String username = "user@phptravels.com";
     public String password = "demouser";
     public static WebDriver driver;
+    public static Logger logger;
 
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
+
+        logger = Logger.getLogger(TCBaseClass.class);
+        PropertyConfigurator.configure("log4j.properties");
     }
 
     @AfterClass
--- selenium_tests/src/test/java/com/selenium_tests/testCases/TC_LoginTest_001.java
+++ selenium_tests/src/test/java/com/selenium_tests/testCases/TC_LoginTest_001.java
@@ -15,16 +15,21 @@ public class TC_LoginTest_001 extends TCBaseClass {
 
     @Test
     public void loginTest() {
-        driver.get(baseURL);
+        driver.get(baseURL);       
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("URL is opened");
 
         LoginPage page = new LoginPage(driver);
         page.setUserName(username);
+        logger.info("Username is entered");
         page.setPassword(password);
+        logger.info("Password is entered");
         page.clickSubmit();
+        logger.info("Submit button pressed");
         
         WebDriverWait waitLogin = new WebDriverWait(driver, 30);
         waitLogin.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//title")));
+        logger.info("Dashboard is loaded");
         Assert.assertEquals(driver.getTitle(), EXPECTED_TITLE);      
     }
 }
